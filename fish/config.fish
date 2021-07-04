b273fe7861ec7d7d49544a1cc3e04dca21ae5503
DiegoAGtz <diegogtz1207@gmail.com>
2021-07-03 23:08:03 -0500

    Se agrego fish shell

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set EDITOR "nvim"                                 # $EDITOR use Emacs in terminal
+
+
+### RANDOM COLOR SCRIPT ###
+colorscript random
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
